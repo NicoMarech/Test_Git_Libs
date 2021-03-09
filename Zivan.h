95f41da49cddd3e5ea9b84c5ece3a70babdca70f
Nicolas Maréchal <nicolas.marechal@novum-tech.fr>
2021-03-09 16:23:43 +0100

    Ajout Zivan

---
+++ Zivan.h
@@ -0,0 +1,25 @@
+#pragma once
+#ifndef ZIVAN_H
+#define ZIVAN_H
+#include <iostream>
+
+inline void ZIVAN_Init()
+{
+    std::cout << "ZIVAN used and init\n";
+}
+
+inline void ZIVAN_Process()
+{
+    std::cout << "ZIVAN Process\n";
+}
+
+inline void ZIVAN_Start()
+{
+    std::cout << "ZIVAN Start\n";
+}
+
+inline void ZIVAN_Stop()
+{
+    std::cout << "ZIVAN Stop\n";
+}
+#endif
